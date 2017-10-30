d8fc090f5d093d173cc20f82ee514f32ef682b7a
Seungmoon Park <uctpsp1@econ.ucl.ac.uk>
2017-10-30 13:37:13 +0000

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def first_function(x):
+    print(x)
+    
+first_function(3)   
---
+++ test1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 13:28:06 2017
+
+@author: uctpsp1
+"""
+
